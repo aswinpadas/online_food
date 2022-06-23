7c31d60f05b0ba649659356b95303352f25832d0
aswin <aswinpdas83@gmail.com>
2022-06-23 10:26:33 +0530

    Todays commits:
    *Cart models.py and Cart Itmes models.py

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import *
+admin.site.register(CartModel)
+admin.site.register(CartItemModel)
--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,21 @@
+from shop.models import *
 from django.db import models
 
 # Create your models here.
+
+
+class CartModel(models.Model):
+    cart_id = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart_id
+
+
+class CartItemModel(models.Model):
+    prod = models.ForeignKey(ProductsModel, on_delete=models.CASCADE)
+    cart = models.ForeignKey(CartModel, on_delete=models.CASCADE)
+    qty = models.IntegerField()
+
+    def __str__(self):
+        return self.prod
