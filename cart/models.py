60e423029415df856ec320a22e7d995513d9f4df
aswin <aswinpdas83@gmail.com>
2022-06-30 13:18:10 +0530

    Todays commits:
    * small change
    * card frame html page created

--- cart/models.py
+++ cart/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class CartModel(models.Model):
     cart_id = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
-
+    #
     # def __str__(self):
     #     return self.cart_id
 
@@ -17,5 +17,5 @@ class CartItemModel(models.Model):
     cart = models.ForeignKey(CartModel, on_delete=models.CASCADE)
     qty = models.IntegerField()
 
-    # def __str__(self):
-    #     return self.prod
+    def __str__(self):
+        return self.prod
--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,8 @@ import random
 from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
 # Create your views here.
+from cart.models import CartItemModel
+from cart.views import c_id
 from shop.models import ProductsModel
 
 
@@ -18,7 +20,10 @@ def home(request):
         pro = paginator.page(page)
     except(EmptyPage, InvalidPage):
         pro = paginator.page(paginator.num_pages)
-    return render(request, 'index.html', {'products': obj_product, 'pg': pro})
+    ct_obj = CartItemModel.objects.all().filter(cart__cart_id=c_id(request))
+
+    # print(str(obj_product),'\n***************************\n',str(ct_obj))
+    return render(request, 'card.html', {'products': obj_product, 'pg': pro,'cart': ct_obj})
 
 def homeFilter(request,filt):
     obj_product = ProductsModel.objects.filter(category__slug=filt)
